8f278b9b8b7e9b9053ba11db5ab0683b4a853af5
Kevin Llorente <kevin.llorente@gmail.com>
2018-04-24 16:08:50 -0700

    final adjustments

--- index.js
+++ index.js
@@ -14,19 +14,21 @@ function getDataFromFoodApi(searchTerm, callback) {
 function renderWrittenResult(result) {
   return `
   <div class="single-result">
-    <h2 class ="js-result-name">
-      <a href="${result.recipe.url}" target= "_blank">${result.recipe.label}</a>   
-    </h2>
-  
-    <a href="${result.recipe.url}" target="_blank"><img src="${result.recipe.image}" class="thumbnail"></a>
-    <div scroll-bar-wrap>
+      <h2 class ="js-result-name">
+        <a href="${result.recipe.url}" target= "_blank" title="${result.recipe.label}">${result.recipe.label} </a>   
+      </h2>
+    <div class="recipeIcons">
+      <a href="${result.recipe.url}" target="_blank"><img src="${result.recipe.image}" class="thumbnail" title="Go to this recipe"></a><!--
+      --><img src = "https://cdn0.iconfinder.com/data/icons/most-usable-logos/120/you_Tube-512.png" class="videoIcon" title="Click here for related Video Recipes!">
+    </div>
+    <div class= "scroll-bar-wrap">
       <div class="ingredientItems scroll-box">
-    <p class="ingredient-ul">Ingredients for this Recipe:
-      ${makeUL(result.recipe.ingredientLines)} </p>
+        <p class="ingredient-ul">Ingredients for this Recipe:
+          ${makeUL(result.recipe.ingredientLines)} 
+        </p>
       </div>
       <div class="cover-bar"></div>
     </div>
-    <img src = "https://cdn0.iconfinder.com/data/icons/most-usable-logos/120/you_Tube-512.png" class="videoIcon">
   </div>
   `; 
 }
@@ -42,7 +44,7 @@ function displayRecipeData(data) {
 
 
 
-//Generate List for ingredient bar? 
+//Generate List for ingredients 
 function makeUL(array) {
   const list = document.createElement('ul'); 
   
@@ -73,9 +75,9 @@ function vidResult(data) {
   const x = data.items.map((item, i) => {
     //item.snippet.thumbnails.medium.url;
     relatedVideos += 
-    `<div class= "videoPopUp scroll-bar-wrap">
+    `<div class= "videoPopUp">
        <h2>   
-        <a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank">${item.snippet.title}</a> 
+        <a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank">${item.snippet.title}</a>
         </h2>
       <a href="https://www.youtube.com/watch?v=${item.id.videoId}" target = "_blank">
       <img src=' ${item.snippet.thumbnails.medium.url} ' class=vidThumbnail>
@@ -83,8 +85,6 @@ function vidResult(data) {
         <p>More From: 
         <a href="https://www.youtube.com/channel/${item.snippet.channelId}" target="_blank">${item.snippet.channelTitle}
         </a></p>
-      </div>
-      
     </div>`;
   });
   $('#vidResult .videoPopUp').remove(); 
@@ -112,14 +112,17 @@ document.getElementById('closeButton').addEventListener('click', function(event)
 //jQuery Events
 //document.ready(() => 
 $('.search-results-written').on('click', '.videoIcon', function() {
-    let vidSearch = $('.js-result-name a', event.target.parentElement).text();
+    let vidSearch = $('.js-result-name a', event.target.parentElement.parentElement).text();
     getDataFromVideoApi(vidSearch, vidResult);
+    
   });
+  
   
 $(document).ready(function () {
     $('div.hidden').fadeIn(1000).removeClass('hidden');
 });
-  
+
+
 
 
 
